da30aecf782e7d8e0dec551ab307bf0a50214fc4
Abilis <abilis@mail.ru>
2016-02-02 18:00:51 +0400

    Почти выполнено домашнее задание

--- index.php
+++ index.php
@@ -1,193 +1,188 @@
+<meta charset = 'utf-8'>
+
 <?php
-echo '1 задание. Обычное ветвление.<br>';
-$a = 18;
-$b = 44;
+//Задание 1
+echo 'Задание 1. Нужно вывести все числа в промежутке от 0 до 100, которые делятся на 3 без остатка.<br>';
 
-if (($a >= 0) && ($b >= 0)) {
-    echo 'Сумма: ' . ($a + $b);
-}
-elseif (($a < 0) && ($b < 0)) {
-    echo 'Разность: ' . ($a - $b);
-}
-else {
-    echo 'Произведение: ' . ($a * $b);
-}
-echo '<br>';
-echo '<br>';
-echo '2 задание. Здесь нужно использовать тернарный оператор.<br>';
-
-$c = ($a > $b) ? $a : $b;
-echo $c;
-
-echo '<br>';
-echo '<br>';
-echo '3 задание. Здесь нужно вывести список чисел от $a до 9 с помощью switch.<br>';
-
-$a = 6;
-switch ($a) {
-    case 0: 
-        echo '0<br>';
-    case 1: 
-        echo '1<br>';
-    case 2: 
-        echo '2<br>';
-    case 3: 
-        echo '3<br>';
-    case 4: 
-        echo '4<br>';    
-    case 5: 
-        echo '5<br>';
-    case 6: 
-        echo '6<br>';
-    case 7: 
-        echo '7<br>';
-    case 8: 
-        echo '8<br>';
-    case 9: 
-        echo '9<br>';
-        break;
-    default:
-        echo 'На вход поступило число не из диапазона :(';
-}
-
-echo '<br>';
-echo '<br>';
-echo '4 задание. Здесь нужно реализовать все математические операции в виде функций с двумя параметрами.<br>';
-
-function sum($a, $b) {
-    return ($a + $b);
-}
-
-function raz($a, $b) {
-    return ($a - $b);
-}
-
-function proiz($a, $b) {
-    return ($a * $b);
-}
-
-function chast($a, $b) {
-    return ($a / $b);
-}
-
-//инициализируем переменные
-$a4 = 13; 
-$b4 = 5;
-
-$sum = sum($a4, $b4);
-$raz = raz($a4, $b4);
-$proiz = proiz($a4, $b4);
-$chast = chast($a4, $b4);
-
-echo 'Сумма ' .$a4 . ' и ' . $b4 . ': ' . $sum . '<br>';
-echo 'Разность ' .$a4 . ' и ' . $b4 . ': '  . $raz . '<br>';
-echo 'Произведение ' .$a4 . ' и ' . $b4 . ': '  . $proiz . '<br>';
-echo 'Частное ' .$a4 . ' и ' . $b4 . ': '  . $chast . '<br>';
-
-echo '<br>';
-echo '<br>';
-echo '5 задание. Здесь нужно реализовать функцию с тремя параметрами для математических операций.<br>';
-
-function mathOperation ($a, $b, $operation) {
-    switch ($operation) {
-        case 'sum':
-            return sum($a, $b);
-            break;
-        case 'raz':
-            return raz($a, $b);
-            break;
-        case 'proiz':
-            return proiz($a, $b);
-            break;
-        case 'chast':
-            return chast($a, $b);
-            break;
-        default:
-            return 'this is not a math operation!';
+for ($i = 1; $i < 100; $i++) {
+    if (($i % 3) == 0 ) {
+        echo $i . ' ';
     }
-} 
- 
-$mathOperation = 'chast';
-$result = mathOperation($a4, $b4, $mathOperation);
-echo $result;
+}
 
 echo '<br>';
 echo '<br>';
-echo '6 задание. Здесь нужно реализовать функцию для возведения в степень числа.<br>';
+//Задание 2
+echo 'Задание 2. Нужно написать функцию для вывода числ от 0 до 10 в особом виде.<br>';
 
-$a6 = 2;
-$n6 = 20;
+$i = 0;
 
-function step ($a, $n) {
-    if (($a > 0) && ($n > 0) && (is_int($a)) && (is_int($n))) {
-            if ($n == 1) {
-                return $a;
-            }
-            else {
-                $res = $a * step($a, ($n - 1));
-                return $res;
-            }
+do {
+    if ($i == 0) {
+        echo $i . ' - это нуль<br>';
+        $i++;
+    }
+    if (($i % 2) == 0) {
+        echo $i . ' - это четное число<br>';
     }
     else {
-        echo "Не удалось вычислить $a в степени $n :(";
+        echo $i . ' - это нечетное число<br>';
     }
-        
+    $i++;
+    
+} while ($i <= 10);
+
+echo '<br>';
+echo '<br>';
+//Задание 3*. 
+echo 'Задание 3*. Вывести с помощью цикла for числа от 0 до 9 не использую тело цикла.<br>';
+
+$x = array(); //вспомогательный массив
+$string_for_help = ''; //вспомогательная строка
+
+for ($i = 0; $i < 10; $x[$i] = $i++) {
+    
 }
 
-$res6 = step($a6, $n6);
-echo $res6;
-
+$string_for_help = implode(', ', $x);
+echo $string_for_help;
 
 echo '<br>';
 echo '<br>';
-echo '7 задание. 2 функции, вычисляющие максимальное и минимальное число. Если произведение чисел больше 100, но меньше 1000, то от большего отнять меньшее и показать. Если произведение чисел больше 1000, то произведение этих чисел разделить на большее из числел.<br>';
+//Задание 4. 
+echo 'Задание 4. Нужно объявить массив, где в качестве ключей использованы названия областей, а в качестве значений - массивы с называниями городов из соответствующей области. Вывести на экран кое-каким хитрым образом.<br>';
 
-$a7 = 100;
-$b7 = 21;
+$a4 = array ('Московская область' => array('Москва', 'Зеленоград', 'Клин'),
+             'Ленинградская область' => array('Санкт-Петербург', 'Всеволожск', 'Павловск', 'Кронштадт'),
+             'Рязанская область' => array('Рязань', 'Сасово', 'Рыбное', 'Тума', 'Шилово')
+            );
 
-function maximum ($a, $b) {
-    if ($a > $b) {
-        return $a;
-    }
-    elseif ($a < $b) {
-        return $b;
-    }
-    else {
-        return $a;
+foreach ($a4 as $index => $city) {
+    echo "<h3>$index:</h3>";
+    foreach ($city as $i) {
+        echo $i . '<br>';
     }
 }
 
-function minimum ($a, $b) {
-    if ($a < $b) {
-        return $a;
-    }
-    elseif ($a > $b) {
-        return $b;
-    }
-    else {
-        return $a;
+echo '<br>';
+echo '<br>';
+//Задание 5*. 
+echo 'Задание 5*. Повторить задание 4, но надо вывести на экран только города, начинающиеся с буквы "С".<br>';
+
+foreach ($a4 as $index => $city) {
+    echo "<h3>$index:</h3>";
+    foreach ($city as $i) {
+        $str5 = "$i";
+       $i2 = substr($str5, 0, 2); //Вытаскиваем первый символ в названии города
+        if ($i2 == 'С') {
+            echo $i . '<br>';
+        }
     }
 }
 
-if ((($a7 * $b7) > 100) && (($a7 * $b7) < 1000)) {
-    $res7 = maximum($a7, $b7) - minimum($a7, $b7);
+echo '<br>';
+echo '<br>';
+//Задание 6. 
+echo 'Задание 6. Объявить массив, индексами которого являются буквы русского языка, а значениями - соответствующие латинские буквосочетания. Затем написать функцию транслитерации строк.<br>';
+
+/*$a6 = array('а' => array("a"),
+            'б' => array("b"),
+           'в' => array("v"),
+           'г' => array("g"),
+           'д' => array("d"),
+           'е' => array("e"),
+           'ё' => array("e"),
+           'ж' => array("j"),
+           'з' => array("z"),
+           'и' => array("i"),
+           'к' => array("k"),
+           'л' => array("l"),
+           'м' => array("m"),
+           'н' => array("n"),
+           'о' => array("o"),
+           'п' => array("p"),
+           'р' => array("r"),
+           'с' => array("s"),
+           'т' => array("t"),
+           'у' => array("u"),
+           'ф' => array("f"),
+           'х' => array("h"),
+           'ц' => array("c"),
+           'ч' => array("ch"),
+           'ш' => array("sh"),
+           'щ' => array("sch"),
+           'ъ' => array("'"),
+           'ы' => array("y"),
+           'ь' => array("'"),
+           'э' => array("e"),
+           'ю' => array("u"),
+           'я' => array("ya"),           
+           );*/
+
+//Функция замена символа русского алфафита на группу английских
+function translit($symbol) {
+    
+$a6 = array('а' => array("a"),
+            'б' => array("b"),
+           'в' => array("v"),
+           'г' => array("g"),
+           'д' => array("d"),
+           'е' => array("e"),
+           'ё' => array("e"),
+           'ж' => array("j"),
+           'з' => array("z"),
+           'и' => array("i"),
+           'к' => array("k"),
+           'л' => array("l"),
+           'м' => array("m"),
+           'н' => array("n"),
+           'о' => array("o"),
+           'п' => array("p"),
+           'р' => array("r"),
+           'с' => array("s"),
+           'т' => array("t"),
+           'у' => array("u"),
+           'ф' => array("f"),
+           'х' => array("h"),
+           'ц' => array("c"),
+           'ч' => array("ch"),
+           'ш' => array("sh"),
+           'щ' => array("sch"),
+           'ъ' => array("'"),
+           'ы' => array("y"),
+           'ь' => array("'"),
+           'э' => array("e"),
+           'ю' => array("u"),
+           'я' => array("ya"),           
+           );
+    
+    $symbol = (string)$symbol; //теперь это точно строка
+    
+    foreach ($a6 as $index => $sym) {
+        if ($index == $symbol) {  
+            $symbol2 = $sym[0];
+        }
+        else {
+            return $symbol;
+        }
     }
-elseif (($a7 * $b7) > 1000) {
-    $res7 = ($a7 * $b7) / maximum($a7, $b7);
+    return $symbol2;
 }
-else {
-    $res = 'Результат не вычисляется :(';
-    echo 'Что-то пошло не так :(';
-}
+
+$testtranslit = '1';
+$testtranslit_length = strlen($testtranslit); //вытаскиваем длину строки
+
+$i = 0;
+
+$i2 = substr($testtranslit, $i, 1); //Вытаскиваем i-ый символ в строке
+
+
+echo translit($testtranslit);
 echo '<br>';
-echo 'Первое число: ' . $a7;
-echo '<br>';
-echo 'Второе число: ' . $b7;
-echo '<br>';
-echo 'Максимальное число: ' . maximum($a7, $b7);
-echo '<br>';
-echo 'Произведение чисел: ' . ($a7 * $b7);
-echo '<br>';
-echo $res7;
+echo $testtranslit_length . '<bf>';
+
+
+
+    
 
 ?>
