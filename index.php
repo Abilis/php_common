a689bb503cec06a123918b9b113f0b6e42967026
Abilis <abilis@mail.ru>
2016-02-05 12:29:49 +0400

    Урок 7. Начало

--- index.php
+++ index.php
@@ -1,45 +1,6 @@
 <?php
-require ('functions.php');
-?>
-
-<html>
-
-<head>
-    <title>Галерея фотографий</title>
-</head>
-
-<body>
-<h1>Галерея фотографий</h1>    
-    
-<?php
-    
-if (isset($_FILES['file'])) {
-    upload_file($_FILES['file']);
-  }  
-else {
-    print_form();
-    } 
 
 
-//Получаем список файлов в каталоге $dir    
-$dir_small = './img_small/';
-$dir = './img/';
-$files = list_files($dir_small);
 
-//Проходим по полученному массиву и гененируем по именам ссылки для картинок
-$n = count($files);
-
-for ($i = 2; $i < $n; $i++) {
-    $temp_img = $dir_small . $files[$i]; ?>
-    <a href="<?=$dir . $files[$i]?>"><img src="<?=$dir_small . $files[$i]?>"></a>
-    <?php echo ''; 
-}
-    
-    
-
-    
 
 ?>
-
-</body>
-</html>
