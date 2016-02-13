95eb450080d439a50580ef7cbe6815c67a66831e
Abilis <abilis@mail.ru>
2016-02-13 20:42:57 +0400

    Дописаны delete.php и update.php

--- Lesson_1/delete.php
+++ Lesson_1/delete.php
@@ -1,6 +1,30 @@
 <?php
-require_once('databace.php');
+require_once('database.php');
 
+//Если ничего не передано в $_GET['id_article'], значит, ничего удалять не надо
+if (!isset($_GET['id_article'])) {
+    header('Location: index.php');
+    exit();
+}
+
+//Формируем запрос
+
+$id_article = $_GET['id_article'];
+$sql = "DELETE FROM articles WHERE id_article = $id_article";
+
+//Выполняем запрос
+$result = mysql_query($sql);
+
+if (!$result) {
+    die('Невозможно удалить статью с таким идентификатором!' . mysql_error());
+}
+
+else {
+    echo 'Статей удалено: ' . mysql_affected_rows();
+}
+
+//Закрываем соединение с БД
+mysql_close();
     
     
     
--- Lesson_1/update.php
+++ Lesson_1/update.php
@@ -1,7 +1,30 @@
 <?php
-require_once('databace.php');
+require_once('database.php');
 
-    
+//Если ничего не передано в $_GET['id_article'], значит, ничего изменять не надо
+if (!isset($_GET['id_article'])) {
+    header('Location: index.php');
+    exit();
+}
+
+//Формируем запрос
+
+$id_article = $_GET['id_article'];
+$sql = "UPDATE articles SET title = 'Измененный заголовок', content = 'Измененное содержимое статьи' WHERE id_article = $id_article";
+
+//Выполняем запрос
+$result = mysql_query($sql);
+
+if (!$result) {
+    die('Невозможно модифицировать статью с таким идентификатором!' . mysql_error());
+}
+
+else {
+    echo 'Статей модифицировано: ' . mysql_affected_rows();
+}
+
+//Закрываем соединение с БД
+mysql_close();    
     
     
 ?>
