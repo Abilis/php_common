597c52a819bd654e2fdbb4dc8c6f02c07a09313c
Abilis <abilis@mail.ru>
2016-02-26 10:41:22 +0400

    level_2-lesson_6

---
+++ Lesson_5/sample5/index.php
@@ -0,0 +1,1 @@
+<?phpinclude_once('inc/C_View.php');include_once('inc/C_Edit.php');switch ($_GET['c']){case 'edit':	$controller = new C_Edit();	break;default:	$controller = new C_View();}$controller->Request();
